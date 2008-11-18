c3b60be0d3872c13ad6c20c4efff5c84438d5c37
Victor Gumayunov <gumayunov@gmail.com>
2008-11-18 17:32:13 +0300

    save

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
-cp -R .vim ~/
-cp -R .tools ~/
-cp .gitconfig ~/
-cp .vimrc ~/
-cp .cshrc ~/
+#!/bin/sh
+
+cp -Rf .vim ~/
+cp -Rf .tools ~/
+cp -f .gitconfig ~/
+cp -f .vimrc ~/
+if [ `uname` == 'Linux' ]; then
+  cp -f .cshrc ~/
+else
+  cp -f .cshrc-bsd ~/.cshrc
+fi
 cp .screenrc ~/ 
